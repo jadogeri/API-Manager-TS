738e7b57cf533b5a617eea5c45f44634a973efef
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-07-14 07:14:01 -0500

    Updated ApiManager - added new {romise object condition

--- src/entities/ApiManager.ts
+++ src/entities/ApiManager.ts
@@ -2,29 +2,29 @@ import Config from "./Config.js";
 import ApiMethods from "../helpers/ApiMethods";
 
 class ApiManager {
-    #config: Config;
+    private config: Config;
 
     constructor({ baseUrl = null, headers = null }: { baseUrl?: string | null; headers?: Record<string, string> | null }) {
-        this.#config = new Config({
+        this.config = new Config({
             baseUrl: baseUrl,
             headers: headers
         });
     }
 
     getConfig = (): Config => {
-        return this.#config;
+        return this.config;
     }
 
     setConfig = (config: Config): void => {
-        this.#config = config;
+        this.config = config;
     }
 
     updateHeader = (headers: Record<string, string>): void => {
-        this.#config.setHeaders(headers);
+        this.config.setHeaders(headers);
     }
 
     updateBaseUrl = (baseUrl: string): void => {
-        this.#config.setBaseUrl(baseUrl);
+        this.config.setBaseUrl(baseUrl);
     }
 
     instance(): ApiManager {
@@ -32,23 +32,23 @@ class ApiManager {
     }
 
     get(endpoint: string): Promise<any> {
-        return ApiMethods.getHandler(endpoint, this.#config);
+        return ApiMethods.getHandler(endpoint, this.config);
     }
 
     put(endpoint: string, data: any): Promise<any> {
-        return ApiMethods.putHandler(endpoint, data, this.#config);
+        return ApiMethods.putHandler(endpoint, data, this.config);
     }  
 
     patch(endpoint: string, data: any): Promise<any> {
-        return ApiMethods.patchHandler(endpoint, data, this.#config);
+        return ApiMethods.patchHandler(endpoint, data, this.config);
     }
 
     post(endpoint: string, data: any): Promise<any> {
-        return ApiMethods.postHandler(endpoint, data, this.#config);   
+        return ApiMethods.postHandler(endpoint, data, this.config);   
     }
 
     delete(endpoint: string): Promise<any> {
-        return ApiMethods.deleteHandler(endpoint, this.#config);
+        return ApiMethods.deleteHandler(endpoint, this.config);
     }
 }
 
